558343fded2ac4671db3fb8a28ea824e00934d99
Zhuxb <ZhuxbClouds@gmail.com>
2022-11-23 11:48:52 +0800

    chore: 新增压缩插件，重新build

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,8 @@
-// /*
-//  * @Author: Zhuxb ZhuxbClouds@gmail.com
-//  * @Date: 2022-07-02 13:13:38
-//  * @LastEditors: Zhuxb ZhuxbClouds@gmail.com
-//  * @LastEditTime: 2022-07-05 16:02:20
-//  * @FilePath: \huashuo-CardList\vue.config.js
-//  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-//  */
-const { defineConfig } = require('@vue/cli-service')
-const AutoImport = require('unplugin-auto-import/webpack')
-const Components = require('unplugin-vue-components/webpack')
-const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
+const { defineConfig } = require("@vue/cli-service");
+const AutoImport = require("unplugin-auto-import/webpack");
+const Components = require("unplugin-vue-components/webpack");
+const { ElementPlusResolver } = require("unplugin-vue-components/resolvers");
+const CompressionPlugin = require("compression-webpack-plugin");
 module.exports = defineConfig({
   transpileDependencies: true,
   configureWebpack: {
@@ -20,6 +13,15 @@ module.exports = defineConfig({
       Components({
         resolvers: [ElementPlusResolver()],
       }),
+      new CompressionPlugin({
+        filename: "[path][base].gz",
+        algorithm: "gzip",
+        test: /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i,
+        threshold: 10240, // 压缩门槛
+        minRatio: 0.8, // 最小压缩比
+        deleteOriginalAssets: true, //是否删除原资源，
+        compressionOptions: { level: 6 },
+      }),
     ],
-  }
-})
+  },
+});
